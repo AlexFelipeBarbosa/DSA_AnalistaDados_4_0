f74c638bfc8f45832ffca1a457e909d825b8c0b1
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2024-10-25 17:36:57 -0300

    Cap22

---
+++ 01_SQLparaAnaliseDeDados/Cap22 - Projeto Especial/ProjetoEspecial.py
@@ -0,0 +1,8 @@
+# Instalando os pacotes 
+# pip install -q langchain==0.1.13
+# pip install -q pymupdf==1.23.19
+# pip install -q huggingface-hub==0.20.3
+# pip install -q faiss-cpu==1.7.4
+# pip install -q sentence-transformers==2.2.2
+# pip install -q openai==1.14.3
+
